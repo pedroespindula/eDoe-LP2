80df909d3f2ea54204e29164b1897d07124ba05a
Pedro Espíndula <joao.espindula@ccc.ufcg.edu.br>
2018-11-18 20:08:18 -0300

    Adiciona suporte para testes de aceitacao

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import easyaccept.EasyAccept;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] easyAccept = new String[]{"edoe.EDoeFacade",
+                //"test/edoe/acceptance/use_case_1.txt",
+                //"test/edoe/acceptance/use_case_2.txt",
+                //"test/edoe/acceptance/use_case_3.txt",
+                //"test/edoe/acceptance/use_case_4.txt",
+                //"test/edoe/acceptance/use_case_5.txt",
+                //"test/edoe/acceptance/use_case_6.txt",
+                //"test/edoe/acceptance/use_case_7.txt",
+        };
+        EasyAccept.main(easyAccept);
+    }
+
+}
---
+++ src/edoe/EDoeFacade.java
@@ -0,0 +1,4 @@
+package edoe;
+
+public class EDoeFacade {
+}
