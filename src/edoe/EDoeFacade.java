d62d2090ac388e783446d161073b4bf55956bf73
Pedro Espíndula <joao.espindula@ccc.ufcg.edu.br>
2018-11-18 20:14:10 -0300

    Adiciona assinatura de metodos da US1

--- src/edoe/EDoeFacade.java
+++ src/edoe/EDoeFacade.java
@@ -1,4 +1,26 @@
 package edoe;
 
 public class EDoeFacade {
+
+    // US1
+    public String adicionaDoador (String id, String nome, String email, String celular, String classe) {
+        return "";
+    }
+
+    public String pesquisaUsuarioPorId (String id) {
+        return "";
+    }
+
+    public String pesquisaUsuarioPorNome (String nome) {
+        return "";
+    }
+
+    public String atualizaUsuario (String id, String nome, String email, String celular) {
+        return "";
+    }
+
+    public void removeUsuario (String id) {
+
+    }
+
 }
