b307bbf4461a18d6e2464129c9d73041f3cb4069
Pedro Espíndula <joao.espindula@ccc.ufcg.edu.br>
2018-11-19 13:41:01 -0300

    Move os testes de aceitacao para pasta externa

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         String[] easyAccept = new String[]{"edoe.EDoeFacade",
-                "test/edoe/acceptance/use_case_1.txt",
-                "test/edoe/acceptance/use_case_2.txt",
-                "test/edoe/acceptance/use_case_3.txt",
-                //"test/edoe/acceptance/use_case_4.txt",
-                //"test/edoe/acceptance/use_case_5.txt",
-                //"test/edoe/acceptance/use_case_6.txt",
-                //"test/edoe/acceptance/use_case_7.txt",
+                "acceptance_tests/use_case_1.txt",
+                "acceptance_tests/use_case_2.txt",
+                "acceptance_tests/use_case_3.txt",
+                //"acceptance_tests/use_case_4.txt",
+                //"acceptance_tests/use_case_5.txt",
+                //"acceptance_tests/use_case_6.txt",
+                //"acceptance_tests/use_case_7.txt",
         };
         EasyAccept.main(easyAccept);
     }
