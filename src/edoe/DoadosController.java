046b9b17803a2b6119cc3fda9ff1539d4f92e843
Vitor Diniz <vitor.diniz96@gmail.com>
2018-11-22 16:16:10 -0300

    **Correcao de metodos**

--- src/edoe/DoadosController.java
+++ src/edoe/DoadosController.java
@@ -14,19 +14,23 @@ public class DoadosController {
   }
 
   public void adicionaDescritor (String descritor) {
-	if (!this.itemsPorDescritor.containsKey(descritor)) {
-	  List <Item> lista = new ArrayList <Item>();
-	  this.itemsPorDescritor.put(descritor, lista);		
+    if (!this.itemsPorDescritor.containsKey(descritor)) {
+      List <Item> lista = new ArrayList <Item>();
+      this.itemsPorDescritor.put(descritor, lista);		
 	}
+    else {
+      throw new IllegalArgumentException();
+    }
   }
 
   public String adicionaItemParaDoacao(int id, Usuario doador, String descricao, int quantidade, String tags) {
     adicionaItemPorDoador(id, doador, descricao, quantidade, tags);
     adicionaItemPorDescritor(id, doador, descricao, quantidade, tags);
-	return "Item cadastrado com sucesso.";
+    String ID = String.valueOf(id);
+    return ID;
   }
   private void adicionaItemPorDoador(int id, Usuario doador, String descricao, int quantidade, String tags) {
-	if (this.itemsPorDoador.containsKey(doador)) {
+    if (this.itemsPorDoador.containsKey(doador)) {
       Item item = new Item (id,descricao, quantidade, tags, doador);
       this.itemsPorDoador.get(doador).add(item);
     }
@@ -38,46 +42,47 @@ public class DoadosController {
     }
   }
   private void adicionaItemPorDescritor(int id, Usuario doador, String descricao, int quantidade, String tags) {
-	if (this.itemsPorDescritor.containsKey(descricao)) {
-	  Item item = new Item (id,descricao, quantidade, tags, doador);
-	  this.itemsPorDescritor.get(descricao).add(item);
-	}
-	else {
-	  List <Item> lista = new ArrayList <Item>();
-	  Item item = new Item (id,descricao, quantidade, tags, doador);
-	  lista.add(item);
-	  this.itemsPorDescritor.put(descricao, lista);
-	}  
+    if (this.itemsPorDescritor.containsKey(descricao)) {
+      Item item = new Item (id,descricao, quantidade, tags, doador);
+      this.itemsPorDescritor.get(descricao).add(item);
+    }
+    else {
+      List <Item> lista = new ArrayList <Item>();
+      Item item = new Item (id,descricao, quantidade, tags, doador);
+      lista.add(item);
+      this.itemsPorDescritor.put(descricao, lista);
+    }  
   }
   
   public String exibeItem (Usuario doador, String id) {
-	Integer ID = Integer.valueOf(id);
-	if (this.itemsPorDoador.containsKey(doador)) {
-	  try {
-      return this.itemsPorDoador.get(doador).get(ID).toString();
-	  } catch (IllegalArgumentException e) {	  }
+    Integer ID = Integer.valueOf(id);
+    if (this.itemsPorDoador.containsKey(doador)) {
+     try {
+     return this.itemsPorDoador.get(doador).get(ID).toString();
+     } catch (IllegalArgumentException e) {	  }
     }
-	return "Item nao encontrado.";
-  }
+    return "Item nao encontrado.";
+ }
 
   public String atualizaItemParaDoacao(Usuario doador, String descricao, int quantidade, String tags, String id) {
-	Integer ID = Integer.valueOf(id);
-	if (this.itemsPorDoador.containsKey(doador)) {
-	  try {
-	  this.itemsPorDoador.get(doador).get(ID).setQuantidade(quantidade);
-	  this.itemsPorDoador.get(doador).get(ID).setTags(tags);
-	  this.itemsPorDescritor.get(descricao).get(ID).setQuantidade(quantidade);
-	  this.itemsPorDescritor.get(descricao).get(ID).setTags(tags);
-	  } catch(IllegalArgumentException e) {   	}
-	}
+    Integer ID = Integer.valueOf(id);
+    if (this.itemsPorDoador.containsKey(doador)) {
+      try {
+      this.itemsPorDoador.get(doador).get(ID).setQuantidade(quantidade);
+      this.itemsPorDoador.get(doador).get(ID).setTags(tags);
+      this.itemsPorDescritor.get(descricao).get(ID).setQuantidade(quantidade);
+      this.itemsPorDescritor.get(descricao).get(ID).setTags(tags);
+      return id;
+      } catch(IllegalArgumentException e) {   	}
+    }
     return "Item nao encontrado.";
   }
 
   public void removeItemParaDoacao(Usuario doador, String id, String descritor) {
-	int ID = Integer.valueOf(id);
+    int ID = Integer.valueOf(id);
     if (this.itemsPorDescritor.containsKey(descritor)) {
-    	this.itemsPorDescritor.get(descritor).remove(ID);
-    	this.itemsPorDoador.get(doador).remove(ID);
+      this.itemsPorDescritor.get(descritor).remove(ID);
+      this.itemsPorDoador.get(doador).remove(ID);
     }
   }
 
