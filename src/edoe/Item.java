dfc2f93ee8a1264ebc9bb905bed29a001244bad9
Vitor Diniz <vitor.diniz96@gmail.com>
2018-11-22 10:53:34 -0300

    Adiciona classe item

--- src/edoe/Item.java
+++ src/edoe/Item.java
@@ -1,47 +1,60 @@
 package edoe;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Item {
 
-  private String descricao;
+  private String descricao; //id
   private int quantidade;
   private List<String> tags;
   private Usuario usuario;
-  private String data;
 
-  public Item(String descricao, int quantidade, List<String> tags, Usuario usuario, String data) {
-
+  public Item(String descricao, int quantidade, List<String> tags, Usuario usuario) {
+      this.descricao = descricao;
+      this.quantidade = quantidade;
+      this.tags = tags;
+      this.usuario = usuario;
   }
 
   public String getDescricao() {
-    return "";
+    return this.descricao;
   }
 
   public Usuario getUsuario() {
-    return null;
+
+      return this.usuario;
   }
 
   public void setQuantidade(int quantidade) {
-
+      this.quantidade = quantidade;
   }
 
   public void setTags(List<String> tags) {
-
+      for (String tag: tags)
+          this.tags.add(tag);
   }
 
   @Override
   public String toString() {
-    return "";
+    return this.descricao + ", tags: " + this.tags + ", " + this.quantidade;
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    return false;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(descricao, item.descricao) &&
+                Objects.equals(tags, item.tags) &&
+                Objects.equals(usuario, item.usuario);
+    }
 
-  @Override
-  public int hashCode() {
-    return 0;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(descricao, tags, usuario);
+    }
+
+
+
 }
