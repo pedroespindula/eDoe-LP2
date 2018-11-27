c8f5249e8381a9f274b5170829a8427365e271d0
Rob <bruno.robsiq@gmail.com>
2018-11-27 06:27:25 -0300

    Atualiza validador
    * Adiciona construtor com msgGeral vazia
    * Torna msgGeral privada
    * Corrige metodos que verificam existencia nos mapas
    * Corrige referencias para this.msgGeral
    * Corrige excecoes em verificaStringVazia
      e verificaStringNula

--- src/util/Validador.java
+++ src/util/Validador.java
@@ -3,8 +3,11 @@ package util;
 import java.util.Map;
 
 public class Validador {
+  private String msgGeral;
 
-  String msgGeral;
+  public Validador() {
+    this.msgGeral = "";
+  }
 
   public Validador(String msgGeral) {
     this.msgGeral = msgGeral + ": ";
@@ -24,32 +27,42 @@ public class Validador {
 
   public void verificaStringVazia(String parametro, String msgGeral, String mensagem) {
     if (parametro.trim().isEmpty()) {
-      throw new IllegalArgumentException(msgGeral + ": " + mensagem);
+      throw new IllegalArgumentException(this.msgGeral + mensagem);
     }
   }
 
   public void verificaNulo(Object parametro, String msgGeral, String mensagem) {
     if (parametro == null) {
-      throw new NullPointerException(msgGeral + ": " + mensagem);
+      throw new NullPointerException(this.msgGeral + mensagem);
     }
   }
-
 
   public void verificaStringNulaOuVazia(String parametro, String mensagem) {
     verificaNulo(parametro, mensagem);
     verificaStringVazia(parametro, mensagem);
   }
 
-  public void verificaContem(Object chave, Map<Object, Object> mapa, String mensagem) {
+  public void verificaContem(Object chave, Map<?, ?> mapa, String mensagem) {
     if (mapa.containsKey(chave)) {
-      throw new IllegalArgumentException(msgGeral + mensagem);
+      throw new IllegalArgumentException(this.msgGeral + mensagem);
     }
   }
 
-  public void verificaNaoContem(Object chave, Map<Object, Object> mapa, String mensagem) {
+  public void verificaNaoContem(Object chave, Map<?, ?> mapa, String mensagem) {
     if (!mapa.containsKey(chave)) {
-      throw new IllegalArgumentException(msgGeral + mensagem);
+      throw new IllegalArgumentException(this.msgGeral + mensagem);
     }
   }
 
+  public void verificaInteiroNegativo(int parametro, String mensagem) {
+    if (parametro < 0) {
+      throw new IllegalArgumentException(this.msgGeral + mensagem);
+    }
+  }
+
+  public void verificaInteiroMaiorQueZero(int parametro, String mensagem) {
+    if (parametro <= 0) {
+      throw new IllegalArgumentException(this.msgGeral + mensagem);
+    }
+  }
 }
