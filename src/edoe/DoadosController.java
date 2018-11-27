127388e24983eac444f7c8bfc7a13efb27a7cc33
Anderson Clemente <anderson.filipe.silva@ccc.ufcg.edu.br>
2018-11-27 09:56:16 -0300

    Modifica metodos de listagens

--- src/edoe/DoadosController.java
+++ src/edoe/DoadosController.java
@@ -1,32 +1,19 @@
 package edoe;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.Map.Entry;
+import java.util.Set;
+
 public class DoadosController {
 
-	private Map<Usuario, List<Item>> itemsPorDoador;
-	private Map<String, List<Item>> itemsPorDescritor;
-	
-	public static void main(String[] args) {
-		DoadosController d = new DoadosController();
-		System.out.println(d.pesquisaItemParaDoacaoPorDescricao("cad"));
-	}
+	private Set<String> descricoes;
+	private Map<Usuario, Map<Integer, Item>> itens;
 
 	public DoadosController() {
-		itemsPorDescritor = new TreeMap<>();
-		itemsPorDescritor.put("cadeira de rodas", new ArrayList<>()); 
-		itemsPorDescritor.put("curso de programacao", new ArrayList<>());
-		itemsPorDescritor.put("cobertor", new ArrayList<>());
-		
-		itemsPorDescritor.get("cadeira de rodas").add(new Item("cadz", 5, "asdas asda", new Usuario("1", "Andre", "asda", "98952", "irra", true)));
-		itemsPorDescritor.get("cadeira de rodas").add(new Item("cada", 3, "asdas asda", new Usuario("1", "Andre", "asda", "98952", "irra", true)));
+		this.itens = new HashMap<>();
+		this.descricoes = new HashSet<>();
 	}
 
 	public void adicionaDescritor(String descritor) {
@@ -50,45 +37,28 @@ public class DoadosController {
 	}
 
 	public String listaDescritorDeItensParaDoacao() {
-		/*List<String> listaDescricoes = new ArrayList<String>();
 
-		for (String chave : itemsPorDescritor.keySet()) {
-			listaDescricoes.add(this.quantidadeDescricao(chave) + " - " + chave);
-		}
+		return descricoes.stream()
+				.sorted()
+				.map(descricao -> itens.values().stream()
+						.flatMap(mapa -> mapa.values().stream())
+						.filter(item -> item.getDescricao().equals(descricao))
+						.map(Item::getQuantidade)
+						.reduce(0, Integer::sum)	+ " - " + descricao)
+				.collect(Collectors.joining(" | "));
 
-		return String.join(" | ",listaDescricoes);
-		*/
-		return this.itemsPorDescritor.entrySet().stream()
-				.map(l -> l.getValue().stream()
-						.map(item -> item.getQuantidade())
-						.reduce(0, Integer::sum) + " - " + l.getKey())
-				.collect(Collectors.joining( " | "));
-	
 	}
 
-	/*private int quantidadeDescricao(String chave) {
-		int somador = 0;
-		List<Item> items = itemsPorDescritor.get(chave);
-		
-		for(Item item: items) {
-			somador += item.getQuantidade();
-		}
-		
-		return somador;
-	}*/
-
 	public String listaItensParaDoacao() {
-		//this.itemsPorDescritor.values().stream().flatMap(List::stream).
 		return null;
 	}
 
 	public String pesquisaItemParaDoacaoPorDescricao(String desc) {
-		return this.itemsPorDescritor.entrySet().stream()
-				.filter(tupla -> tupla.getKey().contains(desc))
-				.map(Entry::getValue)
-				.flatMap(lista -> lista.stream())
-				.sorted(Comparator.comparing(Item::getDescricao))
-				.map(Item::toString).collect(Collectors.joining(" | "));
+		return this.itens.values().stream()
+				.flatMap(mapa -> mapa.values().stream())
+				.filter(item -> item.getDescricao().toLowerCase().trim().contains(desc.toLowerCase().trim()))
+				.map(item -> item.toString())
+				.collect(Collectors.joining(" | "));
 	}
 
 }
