049182987ec539453fc86ba397ecaeed35c49272
Vitor Diniz <vitor.diniz96@gmail.com>
2018-11-27 08:16:40 -0300

    Corrige indentaçoes 

--- src/edoe/DoadosController.java
+++ src/edoe/DoadosController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import util.Validador;
+
 /**
- * controlador dos itens doados 
+ * controlador dos itens doados
+ *
  * @author vitor
  * @author anderson
  */
@@ -15,62 +17,66 @@ public class DoadosController {
   private Map<Usuario, List<Item>> itemsPorDoador;
   private Map<String, List<Item>> itemsPorDescritor;
 
-  public void adicionaDescritor (String descritor) {
+  public void adicionaDescritor(String descritor) {
     if (!this.itemsPorDescritor.containsKey(descritor)) {
-      List <Item> lista = new ArrayList <Item>();
-      this.itemsPorDescritor.put(descritor, lista);		
-	} else {
-      Validador valida = new Validador ("Erro");
+      List<Item> lista = new ArrayList<Item>();
+      this.itemsPorDescritor.put(descritor, lista);
+    } else {
+      Validador valida = new Validador("Erro");
       valida.verificaNulo(this.itemsPorDescritor.get(descritor), "Item inexistente");
     }
   }
 
   public int adicionaItemParaDoacao(int id, Usuario doador, String descricao, int quantidade, String tags) {
-    Item item = new Item (id,descricao, quantidade, tags, doador);
+    Item item = new Item(id, descricao, quantidade, tags, doador);
     adicionaItemPorDoador(id, doador, descricao, quantidade, tags, item);
     adicionaItemPorDescritor(id, doador, descricao, quantidade, tags, item);
     return id;
   }
+
   private void adicionaItemPorDoador(int id, Usuario doador, String descricao, int quantidade, String tags, Item item) {
     if (this.itemsPorDoador.containsKey(doador)) {
       this.itemsPorDoador.get(doador).add(item);
     } else {
-   	  List <Item> lista = new ArrayList <Item>();
+      List<Item> lista = new ArrayList<Item>();
       lista.add(item);
       this.itemsPorDoador.put(doador, lista);
     }
   }
+
   private void adicionaItemPorDescritor(int id, Usuario doador, String descricao, int quantidade, String tags, Item item) {
     if (this.itemsPorDescritor.containsKey(descricao)) {
       this.itemsPorDescritor.get(descricao).add(item);
     } else {
-      List <Item> lista = new ArrayList <Item>();
+      List<Item> lista = new ArrayList<Item>();
       lista.add(item);
       this.itemsPorDescritor.put(descricao, lista);
-    }  
+    }
   }
-  
-  public String exibeItem (Usuario doador, int id) {
+
+  public String exibeItem(Usuario doador, int id) {
     if (this.itemsPorDoador.containsKey(doador)) {
       List<Item> list = this.itemsPorDoador.get(doador);
-        for (Item i: list) 
-          if (id == i.getId())
-            return i.toString();
-       
-     return this.itemsPorDoador.get(doador).get(id).toString();
-    
+      for (Item i : list) {
+        if (id == i.getId()) {
+          return i.toString();
+        }
+      }
+      return this.itemsPorDoador.get(doador).get(id).toString();
     }
     return "Item nao encontrado.";
- }
+  }
 
   public String atualizaItemParaDoacao(Usuario doador, String descricao, int quantidade, String tags) {
     if (this.itemsPorDoador.containsKey(doador)) {
-        for (int i = 0; i < this.itemsPorDoador.get(doador).size(); i++)
-          for (int j = 0; j < this.itemsPorDescritor.get(descricao).size(); j++) 
-            if(this.itemsPorDoador.get(doador).get(i).equals(this.itemsPorDescritor.get(descricao).get(j))) {
-              this.itemsPorDoador.get(doador).get(i).setTags(tags);
-              this.itemsPorDoador.get(doador).get(i).setQuantidade(quantidade);
-            }
+      for (int i = 0; i < this.itemsPorDoador.get(doador).size(); i++){
+        for (int j = 0; j < this.itemsPorDescritor.get(descricao).size(); j++) {
+          if (this.itemsPorDoador.get(doador).get(i).equals(this.itemsPorDescritor.get(descricao).get(j))) {
+            this.itemsPorDoador.get(doador).get(i).setTags(tags);
+            this.itemsPorDoador.get(doador).get(i).setQuantidade(quantidade);
+          }
+        }
+      }
       return "Item atualizado.";
     }
     return "Item nao encontrado.";
@@ -91,7 +97,7 @@ public class DoadosController {
     return "";
   }
 
-  public String pesquisaItemParaDoacaoPorDescricao (String desc) {
+  public String pesquisaItemParaDoacaoPorDescricao(String desc) {
     return "";
   }
 
