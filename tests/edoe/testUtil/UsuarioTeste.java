672e6ad496598b1d13d291cafd3791ffcbda50ab
Rob <bruno.robsiq@gmail.com>
2018-11-29 08:17:08 -0300

    Adiciona construtor com id em UsuarioTeste

--- tests/edoe/testUtil/UsuarioTeste.java
+++ tests/edoe/testUtil/UsuarioTeste.java
@@ -6,4 +6,8 @@ public class UsuarioTeste extends Usuario {
   public UsuarioTeste() {
     super("12345", "Teste", "teste@teste.com", "999", "PESSOA", false);
   }
+
+  public UsuarioTeste(String id) {
+    super(id, "Teste", "teste@teste.com", "999", "PESSOA", false);
+  }
 }
