2bb2cb92356e50a7969475b315853ebe99bc0b3a
Vitor Diniz <vitor.diniz96@gmail.com>
2018-11-29 19:51:09 -0300

    cria Itens em ItemTest

--- tests/edoe/testUtil/ItemTest.java
+++ tests/edoe/testUtil/ItemTest.java
@@ -15,6 +15,12 @@ public class ItemTest {
     Item item = new Item (1, "camisa", 100, "mulambenta, feia, velha", new UsuarioTeste());
     this.item.add(item);
     item = new Item (2, "camisa", 150, "bonita, cheirosa, arrumada", new UsuarioTeste());
-    
+    this.item.add(item);
+    item = new Item (3, "sapato", 12, "estranho, fedido, acabado", new UsuarioTeste());
+    this.item.add(item);
+    item = new Item (4, "camisa", 150, "elegante, bonito, engraxado", new UsuarioTeste());
+    this.item.add(item);
   }
+
+
 }
