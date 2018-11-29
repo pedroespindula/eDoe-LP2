623e1b784fec9a12c4b0e1aa8193a0347bb0a552
Vitor Diniz <vitor.diniz96@gmail.com>
2018-11-29 20:36:50 -0300

    acrescenta javadoc em Item e DoadosController

--- src/edoe/Item.java
+++ src/edoe/Item.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Abstraçao de um item no sistema.
+ *
+ * @author Vitor Diniz - 118110145
+ *
+ */
 public class Item {
   private int id;
   private String descricao;
@@ -11,6 +17,14 @@ public class Item {
   private List<String> tags;
   private Usuario usuario;
 
+  /**
+   * cria um item com seus devidos atributos
+   * @param id
+   * @param descricao
+   * @param quantidade
+   * @param tags
+   * @param usuario
+   */
   public Item(int id, String descricao, int quantidade, String tags, Usuario usuario) {
     this.id = id;
     this.descricao = descricao.toLowerCase();
@@ -19,26 +33,50 @@ public class Item {
     this.usuario = usuario;
   }
 
+  /**
+   * informa o numero identificador (id) do item
+   * @return id
+   */
   public int getId() {
     return this.id;
   }
 
+  /**
+   * informa a descrição do item
+   * @return descrição
+   */
   public String getDescricao() {
     return this.descricao;
   }
 
+  /**
+   * informa qual usuário está relacionado ao item
+   * @return usuario
+   */
   public Usuario getUsuario() {
     return this.usuario;
   }
 
+  /**
+   * informa a quantidade de itens iguais a este existem
+   * @return quantidade
+   */
   public int getQuantidade() {
     return this.quantidade;
   }
 
+  /**
+   * altera a quantidade de itens iguais a este existem
+   * @param quantidade
+   */
   public void setQuantidade(int quantidade) {
     this.quantidade = quantidade;
   }
 
+  /**
+   * altera as tags do item
+   * @param tags
+   */
   public void setTags(String tags) {
     this.tags = Arrays.asList(tags.split(","));
   }
@@ -66,7 +104,11 @@ public class Item {
   private String tagsEmString() {
     return "[" + String.join(", ", this.tags) + "]";
   }
-  
+
+  /**
+   * informa a identificação do usuario ligado ao item
+   * @return identificação do usuario
+   */
   public String getUsuarioIdentificacao() {
 	return this.usuario.getIdentificacao();
   }
