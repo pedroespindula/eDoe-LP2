a19ff7004df76802328f287721981266df215a47
Rob <bruno.robsiq@gmail.com>
2018-11-29 17:44:16 -0300

    Corrige teste de unidade de listagem
    * Espera que a lista seja ordenada pelo item.id

--- test/edoe/NecessitadoControllerTest.java
+++ test/edoe/NecessitadoControllerTest.java
@@ -74,17 +74,17 @@ class NecessitadoControllerTest {
   }
 
   @Test
-  void listaTodosPorQuantidade() {
-    var id3 = this.controller.cadastraItemPedido(new UsuarioTeste(), "cadeira de rodas",
-      11, "grande,adulta,primeira");
+  void listaTodosPorOrdemId() {
     var id1 = this.controller.cadastraItemPedido(new UsuarioTeste(), "cadeira de rodas",
-      9, "grande,adulta,ultima");
+      11, "grande,adulta,primeira");
     var id2 = this.controller.cadastraItemPedido(new UsuarioTeste(), "cadeira de rodas",
-      10, "grande,adulta,segunda");
+      9, "grande,adulta,segunda");
+    var id3 = this.controller.cadastraItemPedido(new UsuarioTeste(), "cadeira de rodas",
+      10, "grande,adulta,ultima");
 
-    var esperado = id3 + " - cadeira de rodas, tags: [grande, adulta, primeira], quantidade: 11, Receptor: Teste/12345 | " +
-                id2 + " - cadeira de rodas, tags: [grande, adulta, segunda], quantidade: 10, Receptor: Teste/12345 | " +
-                id1 + " - cadeira de rodas, tags: [grande, adulta, ultima], quantidade: 9, Receptor: Teste/12345";
+    var esperado = id1 + " - cadeira de rodas, tags: [grande, adulta, primeira], quantidade: 11, Receptor: Teste/12345 | " +
+                id2 + " - cadeira de rodas, tags: [grande, adulta, segunda], quantidade: 9, Receptor: Teste/12345 | " +
+                id3 + " - cadeira de rodas, tags: [grande, adulta, ultima], quantidade: 10, Receptor: Teste/12345";
 
     assertEquals(esperado, this.controller.listaTodos());
   }
