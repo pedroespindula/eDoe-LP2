88189b90605cce875442fcf9d4355908cfc3fc11
Rob <bruno.robsiq@gmail.com>
2018-11-29 07:46:50 -0300

    Adiciona validacoes

--- src/edoe/NecessitadoController.java
+++ src/edoe/NecessitadoController.java
@@ -16,12 +16,18 @@ public class NecessitadoController {
   }
 
   public String cadastraItemPedido(Usuario receptor, String descritor, int quantidade, String tags) {
+    // Validacao
+    var validador = new Validador("Entrada invalida");
+    validador.verificaStringNulaOuVazia(descritor, "descricao nao pode ser vazia ou nula.");
+    validador.verificaInteiroMaiorQueZero(quantidade, "quantidade deve ser maior que zero.");
+
     var items = this.itemsPorReceptor.getOrDefault(receptor, new HashMap<>());
-    var itemTemp = new Item(contador, descritor, quantidade, tags, receptor);
+    var itemTemp = new Item(this.contador, descritor, quantidade, tags, receptor);
     items.putIfAbsent(itemTemp.getId(), itemTemp);
 
     this.itemsPorReceptor.put(receptor, items);
 
+    this.contador += 1;
     return String.valueOf(itemTemp.getId());
   }
 
@@ -35,7 +41,8 @@ public class NecessitadoController {
   }
 
   public String atualizaItem(Usuario receptor, String idItem, int quantidade, String tags) {
-    var item = this.itemsPorReceptor.get(receptor).get(Integer.parseInt(idItem));
+    var id = Integer.parseInt(idItem);
+    var item = this.getItem(receptor, id);
 
     if (quantidade > 0) {
       item.setQuantidade(quantidade);
@@ -51,4 +58,15 @@ public class NecessitadoController {
       this.itemsPorReceptor.get(receptor).remove(Integer.parseInt(idItem));
   }
 
+  private Item getItem(Usuario receptor, int id) {
+    // Validacao
+    var validador = new Validador();
+    validador.verificaInteiroNegativo(id, "Entrada invalida: id do item nao pode ser negativo.");
+    validador.verificaContem(receptor, this.itemsPorReceptor, "O Usuario nao possui itens cadastrados.");
+
+    var itemsUsuario = this.itemsPorReceptor.get(receptor);
+    validador.verificaContem(id, itemsUsuario, "Item nao encontrado: " + id);
+
+    return itemsUsuario.get(id);
+  }
 }
