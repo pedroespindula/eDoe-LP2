363dccdacfd76d787d148745d7e2ba2df17e414b
Anderson Clemente <anderson.filipe.silva@ccc.ufcg.edu.br>
2018-11-29 21:30:50 -0300

    implementa testes de unidade para US3

--- test/edoe/DoadosControllerTest.java
+++ test/edoe/DoadosControllerTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,6 +28,81 @@ class DoadosControllerTest {
 	void testaPesquisaItemParaDoacaoVazio() {
 		assertEquals(doados.pesquisaItemParaDoacaoPorDescricao("a"), "");
 	}
+	
+	@Test
+	void testaPesquisaItemParametroVazio() {
+		try {
+			doados.pesquisaItemParaDoacaoPorDescricao("       ");
+			fail("DEVIA TER QUEBRADO");
+		}catch(IllegalArgumentException o) {
+			assert(true);
+		}
+	}
+	
+
+	@Test
+	void testaPesquisaItemParametroNulo() {
+		try {
+			doados.pesquisaItemParaDoacaoPorDescricao(null);
+			fail("DEVIA TER QUEBRADO");
+		}catch(IllegalArgumentException o) {
+			assert(true);
+		}
+	}
+	
+	@Test
+	void testaListaDescritoresUm() {
+	    this.doados.adicionaDescritor("bola de tenis");
+		assertEquals(doados.listaDescritorDeItensParaDoacao(), "0 - bola de tenis");
+	}
+	
+	@Test
+	void testaListaDescritoresDois() {
+	    this.doados.adicionaDescritor("camisa");
+	    this.doados.adicionaDescritor("bola de tenis");
+		assertEquals(doados.listaDescritorDeItensParaDoacao(), "0 - bola de tenis | 0 - camisa");
+	}
+	
+	@Test
+	void testaListaDescritoresComItens() {
+	    
+		this.doados.adicionaDescritor("camisa");
+	    this.doados.adicionaDescritor("bola de tenis");
+	    
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(), "camisa", 10, "M, BL");
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(), "bola de tenis", 5, "M, BL");
+	
+		assertEquals(doados.listaDescritorDeItensParaDoacao(), "5 - bola de tenis | 10 - camisa");
+	}
+	
+	@Test
+	void testaItensParaDoacaoAlguns() {
+		this.doados.adicionaDescritor("camisa");
+	    this.doados.adicionaDescritor("bola de tenis");
+	    
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(1), "camisa", 3, "M, BL");
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(2), "bola de tenis", 5, "M, BL");
+		
+		assertEquals(doados.listaItensParaDoacao(), "bola de tenis, tags: [M, BL], quantidade: 5, doador: Teste/2 | camisa, tags: [M, BL], quantidade: 3, doador: Teste/1");
+	}
+	
+	@Test
+	void testaPesquisaItemParaDoacaoUnico() {
+		this.doados.adicionaDescritor("camisa");
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(1), "camisa", 10, "M, BL");
+		assertEquals(doados.pesquisaItemParaDoacaoPorDescricao("   CAM        "), "camisa, tags: [M, BL], quantidade: 10");
+	}
+	
+	@Test
+	void testaPesquisaItemParaDoacaoDois() {
+		this.doados.adicionaDescritor("camisa");
+		this.doados.adicionaDescritor("camisola");
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(2), "camisola", 1, "M, BL");
+		this.doados.adicionaItemParaDoacao(new UsuarioTeste(1), "camisa", 3, "M, BL");
+		assertEquals(doados.pesquisaItemParaDoacaoPorDescricao("      CaM      "), "camisa, tags: [M, BL], quantidade: 3 | camisola tags: [M, BL], quantidade: 1");
+	}
+	
+	
 	
 
 }
