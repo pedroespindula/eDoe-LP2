6fcf654aa3415a47790a82e850af6c9f1f6806f0
Anderson Clemente <anderson.filipe.silva@ccc.ufcg.edu.br>
2018-12-13 07:07:38 -0300

    Continua implementacao

---
+++ src/edoe/Persistencia.java
@@ -0,0 +1,27 @@
+package edoe;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Persistencia {
+	public void salvar(Object salvavel, String caminho) throws IOException {
+		FileOutputStream arquivoSaida = new FileOutputStream(caminho);
+		ObjectOutputStream saida = new ObjectOutputStream(arquivoSaida);
+		saida.writeObject(salvavel);
+		saida.close();
+		arquivoSaida.close();
+
+	}
+
+	public Object iniciar(String caminho) throws IOException, ClassNotFoundException {
+		FileInputStream arquivoEntrada = new FileInputStream(caminho);
+		ObjectInputStream entrada = new ObjectInputStream(arquivoEntrada);
+		Object objetoRetorno = entrada.readObject(); 
+		entrada.close();
+		arquivoEntrada.close();
+		return objetoRetorno;
+	}
+}
